ea4619144742a6eecdf31f5a7bfbb4239e79dceb
odiego-041 <162119785+odiego-041@users.noreply.github.com>
2024-11-11 09:03:05 -0300

    Add files via upload

---
+++ CART-O-main/perguntas.js
@@ -0,0 +1,37 @@
+//conteudo
+
+//variavel
+
+//funçao
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    "Perguntas"
+    "Qual maior time do Brasil?"
+    "E o corinthians!!"
+
+)
+
+criaCartao(
+    "Perguntas"
+    "Qual a maior torcida do Brasil?"
+    "E a do gaviao da fiel"
+)
+
